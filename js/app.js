8832c124869495e353b19c0d0238d538c4ee5d97
Andrea Zorzi <andrea.zorzi.94@gmail.com>
2014-12-02 21:31:44 +0100

    Minor fix

--- js/app.js
+++ js/app.js
@@ -1,16 +1,23 @@
-
 $(document).ready(function(){
-    // visualizzazione titolo 'PORTS' completo
-    $(".brandtitle").hover(function(){
 
-        $(".brandtxt").show(500);
-    },
-                           function(){
-        $(".brandtxt").hide(2000);
+    // visualizzazione titolo 'PORTS' completo
+    var mq= window.matchMedia("(min-width: 945px)");
+
+    mq.addListener(function(changed) {
+        if(changed.matches) {
+            $(".brandtitle").hover(function(){
+
+                $(".brandtxt").show(500);
+            },
+                                   function(){
+                $(".brandtxt").hide(2000);
+            });
+        } else {
+            $(".brandtitle").hover(function(){
+            });
+        }
     });
     //END visualizzazione titolo 'PORTS' completo
-
-
 });
 
 
