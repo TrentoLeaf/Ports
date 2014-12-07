23f813b29daef84725951e144382ae00eca4c4be
Davide Pedranz <davide.pedranz@gmail.com>
2014-12-07 23:14:35 +0100

    Improved DateUtilities.

--- js/date.js
+++ js/date.js
@@ -1,15 +1,38 @@
 DateUtilities = {
 
+    addDay : function(date, days) {
+        date.setHours(date.getHours() + 24*days);
+    },
+
     roundToFollowingHalfHour : function(date) {
-        //        date.setSeconds(0);
+        date.setMilliseconds(0);
+        date.setSeconds(0);
 
         if(date.getMinutes() < 30) {
             date.setMinutes(30);
         }
+
         if(date.getMinutes() > 30) {
             date.setMinutes(30);
             date.setHours(date.getHours() + 1);
         }
+    },
+
+    // TODO: handle time before opening
+    nextOpenTime: function(date) {
+        var changed = false;
+
+        if(date.getHours() >= 20) {
+            date.setHours(8 + 24);
+            changed = true;
+        }
+
+        if(date.getDay() == 0) {
+            date.setHours(date.getHours() + 24);
+            changed = true;
+        }
+
+        return changed;
     }
 
 };
