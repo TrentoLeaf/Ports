fa8b88cac504f28d6726aa646d2337e2ac18108e
Andrea <andrea.zorzi.94@gmail.com>
2014-12-11 21:49:02 +0100

    Scheda "Mappa" totalmente funzionante (anche mobile)

--- js/mapController.js
+++ js/mapController.js
@@ -15,64 +15,111 @@ var lastFill=null;
 
         var elabotate = function(array) {
 
-            // TODO: codide jQuery qua!
+            //load mappe
 
-            for(var i = 0, len = array.length; i < len; i++) {
+            var snapP1PT = Snap("#SvgPovo1PT");
+            Snap.load("../img/mappe/Povo1PT.svg", function(data) {
+                gestisciOggetti (data);
+                snapP1PT.append(data);
+            }) ;
+            var snapP1P1 = Snap("#SvgPovo1P1");
+            Snap.load("../img/mappe/Povo1P1.svg", function(data) {
+                gestisciOggetti (data);
+                snapP1P1.append(data);
+            } ) ;
+            var snapP2PT = Snap("#SvgPovo2PT");
+            Snap.load("../img/mappe/Povo2PT.svg", function(data) {
+                gestisciOggetti (data);
+                snapP2PT.append(data);
+            } ) ;
+            var snapP2P1= Snap("#SvgPovo2P1");
+            Snap.load("../img/mappe/Povo2P1.svg",function(data) {
+                gestisciOggetti (data);
+                snapP2P1.append(data);
+            } ) ;
 
-                var className=array[i].number;
-                var idClassNameSvg = "#"+className.toLowerCase();
-                var status=null;
 
-                switch(array[i].class) {
-                    case 'dark-green':
-                        status = colDarkGreen;
-                        break;
+            // funzione di gestione colorazione + append mappe
 
-                    case 'green':
-                        status = colGreen;
-                        break;
+            function gestisciOggetti (data) {
+                for(var i = 0, len = array.length; i < len; i++) {
 
-                    case 'orange':
-                        status = colOrange;
-                        break;
+                    var className=array[i].number;
+                    var idClassNameSvg = "#"+className.toLowerCase();
+                    var rect= data.select(idClassNameSvg);
 
-                    case 'yellow':
-                        status = colYellow;
-                        break;
+                    var status=null;
+                    if (rect!=null){
 
-                    case 'red':
-                        status = colRed;
-                        break;
+                        switch(array[i].class) {
+                            case 'dark-green':
+                                status = colDarkGreen;
+                                break;
+
+                            case 'green':
+                                status = colGreen;
+                                break;
+
+                            case 'orange':
+                                status = colOrange;
+                                break;
+
+                            case 'yellow':
+                                status = colYellow;
+                                break;
+
+                            case 'red':
+                                status = colRed;
+                                break;
+                        }
+
+                        //colore rect
+
+                        rect.attr('fill', status);
+
+                        //colore label
+                        var idLabelSvg= idClassNameSvg+"t";
+                        var label= data.select(idLabelSvg);
+                        if (status==colGreen || status==colRed) {
+                            label.attr('fill', "white");
+                        }
+                        else {
+                            label.attr('fill', "#lalala");
+                        }
+
+                        //hover
+                        rect.hover(function() {
+                            lastFill=  this.attr('fill');
+                            this.attr('fill', colBlue);
+                            this.transform( 's1.3,1.3');
+                        },
+                                   function() {
+                            this.attr('fill', lastFill);
+                            lastFill= null;
+                            this.transform( 's1,1');
+                        }
+                                  );
+
+                        //link ai dettagli di un aula
+                        rect.click(function() {
+                            var aula= (this.attr('id')).toUpperCase();
+                            location.href = "#/details/"+aula;
+                        });
+
+                    }
                 }
-
-                $(idClassNameSvg).css('fill', status);
-
-                if (status==colGreen || status==colRed) {
-                    var idLabelSvg= idClassNameSvg+"t";
-                    $(idLabelSvg).css('fill', "white");
-                }
-                else {
-                    var idLabelSvg= idClassNameSvg+"t";
-                    $(idLabelSvg).css('fill', "#lalala");
-                }
-
-                $(idClassNameSvg).mouseover(function() {
-                    lastFill=  $(this).css('fill');
-                    $(this).css('fill', colBlue);
-                });
-
-                $(idClassNameSvg).mouseleave(function() {
-                    $(this).css('fill', lastFill);
-                    lastFill= null;
-                });
-
-                $(idClassNameSvg).click(function() {
-                    //link ai dettagli di un aula
-                    var aula= ($(this).attr('id')).toUpperCase();
-                     location.href = "#/details/"+aula;
-                  });
-
             }
+
+
+
+
+
+
+
+
+
+
+
         };
 
 
