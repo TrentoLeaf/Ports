442bfa1af9d976676581bd43bd3a654e3cbe41c2
SamDja <SamDja@github.com>
2014-12-15 08:54:02 +0100

    Fix last minute

--- js/controllers.js
+++ js/controllers.js
@@ -24,12 +24,12 @@
     var searchModule = angular.module('searchModule', []);
 
     searchModule.filter('myfilter', function() {
-        return function(items, building, floor, room) {
+        return function(items, building, floor, room, power) {
             var filtered = [];
 
             for(var i = 0, len = items.length; i < len; i++) {
                 var item = items[i];
-                if (item.availability !== 0) {
+                if (item.power == power || power == 'all') {
                     if(item.building == building || building == 'all') {
                         if(item.floor == floor || floor == 'all') {
                             if(room == undefined || room == 'all' || item.number == room) {
@@ -37,14 +37,14 @@
                             }
                         }
                     }
-                }
+               }
             }
 
             return filtered;
         };
     });
 
-    searchModule.controller('SearchController', ['$scope', function($scope) {
+    searchModule.controller('SearchController', ['$scope', '$log', function($scope, $log) {
 
         // in caso di errore reindirizza alla pagina di errore
         if($scope.error) {
@@ -59,6 +59,8 @@
 
         $scope.building = 'all';
         $scope.floor = 'all';
+        
+        $scope.power = true;
 
         $scope.selectBuilding = function(value) {
             $scope.building = value;
@@ -67,6 +69,37 @@
         $scope.selectFloor = function(value) {
             $scope.floor = value;
         };
+        
+         $scope.selectPower = function(value) {
+            $scope.power = value;
+        };
+        
+        var elaborate = function() {
+            $log.warn("Elaborate");
+            $log.warn($scope.rooms);
+            
+            $scope.roomNumbers = [];
+            for(var i = 0, len = $scope.rooms.length; i < len; i++) {
+                $scope.roomNumbers.push($scope.rooms[i].number);
+            }
+        }
+        
+        // aspetta che vengano caricati i dati... appena pronti, chiama elaborate
+        if($scope.loading) {
+            $log.warn("Data loadng... WAIT!");
+
+            $scope.$watch('loading', function(newValue, oldValue) {
+
+                if(!newValue && oldValue) {
+                    $log.info("Data loaded!");
+                    elaborate();
+                }
+            });
+
+        } else {
+            $log.info("Data already loaded...");
+            elaborate();
+        }
 
     }]);
 })();
